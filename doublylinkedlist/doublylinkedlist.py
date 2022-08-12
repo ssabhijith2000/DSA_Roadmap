66b506da726710120b2194cc071c146233240fd5
ssabhijith2000 <ssabhijith2000@gmail.com>
2022-08-13 00:09:12 +0530

    completed doubly linked list

--- doublylinkedlist/doublylinkedlist.py
+++ doublylinkedlist/doublylinkedlist.py
@@ -1,4 +1,7 @@
+from platform import node
+import re
 from telnetlib import DO
+from unittest import defaultTestLoader
 
 
 class Node:
@@ -52,6 +55,75 @@ class DoublyLinkedList:
                 newNode.next = tempNode.next
                 newNode.next.prev = newNode
                 tempNode.next = newNode
+
+    def traverseDLL(self):
+        if self.head is None:
+            print("there is not any element to traverse")
+        else:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value)
+                tempNode = tempNode.next
+
+    def reverseTraverse(self):
+        if self.head is None:
+            print("there is not any element to traverse")
+        else:
+            tempNode = self.tail
+            while tempNode:
+                print(tempNode.prev)
+                tempNode = tempNode.prev
+
+    def searchDLL(self, nodeValue):
+        if self.head is None:
+            print("there is not any element to traverse")
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == nodeValue:
+                    return tempNode.value
+                tempNode = tempNode.next
+            return "the node does not eist in this list"
+
+    def delete(self, location):
+        if self.head is None:
+            print("there is not any element to traverse")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.head.prev = None
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+            else:
+                currentNode = self.head
+                index = 0
+                while index < location - 1:
+                    currentNode = currentNode.next
+                    index += 1
+                currentNode.next = currentNode.next.next
+                currentNode.next.prev = currentNode
+            print("Successfully defaultTestLoader")
+
+    def deleteentiredll(self):
+        if self.head is None:
+            print("there is not any element to traverse")
+        else:
+            tempNode = self.head
+            while tempNode:
+                tempNode.prev = None
+                tempNode = tempNode.next
+            self.head = None
+            self.tail = None
+            print("dll deleted")
 
 
 doublyll = DoublyLinkedList()
