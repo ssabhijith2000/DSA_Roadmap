78c2a9da9ebf549a3c0c3fa0f87f0e015481d4f7
ssabhijith2000 <ssabhijith2000@gmail.com>
2022-08-15 11:31:33 +0530

    added queue no size

---
+++ queue/queue_no_size.py
@@ -0,0 +1,38 @@
+class Queue:
+    def __init__(self) -> None:
+        self.items = []
+
+    def __str__(self) -> str:
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
+
+    def isEmpty(self):
+        if self.items == []:
+            return True
+        else:
+            return False
+
+    def enqueue(self, value):
+        self.items.append(value)
+        return "The element is inserted at the end of Queue"
+
+    def dequeue(self):
+        if self.isEmpty():
+            return "The element is not in the queue"
+        else:
+            # if 0 is given as a parameter in pop, first element is deleted
+            return self.items.pop(0)
+
+    def peek(self):
+        if self.isEmpty():
+            return "The element is not in the queue"
+        else:
+            return self.items[0]
+
+
+customQueue = Queue()
+customQueue.enqueue(1)
+customQueue.enqueue(2)
+customQueue.enqueue(3)
+customQueue.dequeue()
+print(customQueue)
